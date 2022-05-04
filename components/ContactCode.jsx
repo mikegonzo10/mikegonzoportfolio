6b0d18eaeb9b5e59c390c19e9e36b917077bfd3f
Mike Gonzalez <gonzo@Mikes-MacBook-Air-2.local>
2022-05-04 11:07:57 -0700

    Created my own repo

--- components/ContactCode.jsx
+++ components/ContactCode.jsx
@@ -8,26 +8,26 @@ const ContactCode = () => {
       </p>
       <p className={styles.line}>
         &nbsp;&nbsp;&nbsp;website:{' '}
-        <a href="https://nitinranganath.me" target="_blank" rel="noopener">
-          nitinranganath.me
+        <a href="https://mikegonzalezproject.com" target="_blank" rel="noopener">
+          mikegonzalezprojects.com
         </a>
         ;
       </p>
       <p className={styles.line}>
         &nbsp;&nbsp;&nbsp;email:{' '}
         <a
-          href="mailto:nitinranganath@gmail.com"
+          href="mailto:mikegonzalez4444@gmail.com"
           target="_blank"
           rel="noopener"
         >
-          nitinranganath@gmail.com
+          mikegonzalez4444@gmail.com
         </a>
         ;
       </p>
       <p className={styles.line}>
         &nbsp;&nbsp;&nbsp;github:{' '}
-        <a href="https://github.com/itsnitinr" target="_blank" rel="noopener">
-          itsnitinr
+        <a href="https://github.com/mikegonzo10" target="_blank" rel="noopener">
+          mikegonz10
         </a>
         ;
       </p>
@@ -43,28 +43,28 @@ const ContactCode = () => {
         ;
       </p>
       <p className={styles.line}>
-        &nbsp;&nbsp;&nbsp;twitter:{' '}
+        &nbsp;&nbsp;&nbsp;facebook:{' '}
         <a
-          href="https://www.twitter.com/iamnitinr"
+          href="https://www.facebook.com/profile.php?id=100022111718046"
           target="_blank"
           rel="noopener"
         >
-          iamnitinr
+          mikegonzalez
         </a>
         ;
       </p>
       <p className={styles.line}>
         &nbsp;&nbsp;&nbsp;instagram:{' '}
         <a
-          href="https://www.instagram.com/iamnitinr"
+          href="https://www.instagram.com/mikegon_10"
           target="_blank"
           rel="noopener"
         >
-          iamnitinr
+          mikegon_10
         </a>
         ;
       </p>
-      <p className={styles.line}>
+      {/* <p className={styles.line}>
         &nbsp;&nbsp;&nbsp;polywork:{' '}
         <a
           href="https://www.polywork.com/nitinranganath"
@@ -74,22 +74,22 @@ const ContactCode = () => {
           nitinranganath
         </a>
         ;
-      </p>
+      </p> */}
       <p className={styles.line}>
         &nbsp;&nbsp;&nbsp;telegram:{' '}
-        <a href="https://t.me/iamnitinr" target="_blank" rel="noopener">
-          iamnitinr
+        <a href="https://t.me/mikegonzo10" target="_blank" rel="noopener">
+          mikegonzo10
         </a>
         ;
       </p>
-      <p className={styles.line}>
+      {/* <p className={styles.line}>
         &nbsp;&nbsp;codepen:{' '}
         <a href="https://codepen.io/itsnitinr" target="_blank" rel="noopener">
           nitinranganath
         </a>
         ;
-      </p>
-      <p className={styles.line}>
+      </p> */}
+      {/* <p className={styles.line}>
         &nbsp;&nbsp;codesandbox:{' '}
         <a
           href="https://codesandbox.io/u/itsnitinr"
@@ -99,7 +99,7 @@ const ContactCode = () => {
           itsnitinr
         </a>
         ;
-      </p>
+      </p> */}
       <p className={styles.line}>&#125;</p>
     </div>
   );
--- pages/about.jsx
+++ pages/about.jsx
@@ -2,6 +2,7 @@ const AboutPage = () => {
   return (
     <>
       <h1>About Page</h1>
+      <p> I am a aspiring Frontend/Web/Software developer who love to create projects</p>
     </>
   );
 };
